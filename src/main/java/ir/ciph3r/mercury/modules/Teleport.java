c58e2f9b9e63b4ad7e8157372088b3b2670e6023
BuildTools <unconfigured@null.spigotmc.org>
2022-02-27 14:56:11 +0330

    - Make teleport available for Console

--- src/main/java/ir/ciph3r/mercury/modules/Teleport.java
+++ src/main/java/ir/ciph3r/mercury/modules/Teleport.java
@@ -24,16 +24,15 @@ public class Teleport extends ModuleBase {
             return true;
         }
 
-        if (!(sender instanceof Player)) {
-            Utils.sendColorizedMessage(sender, Messages.NO_CONSOLE);
-            return true;
-        }
-        Player player = (Player) sender;
-
         if (args.length == 0) {
-            Utils.sendColorizedMessage(player, Messages.TELEPORT_USAGE);
+            Utils.sendColorizedMessage(sender, Messages.TELEPORT_USAGE);
             return true;
         } else if (args.length == 1) {
+            if (!(sender instanceof Player)) {
+                Utils.sendColorizedMessage(sender, Messages.NO_CONSOLE);
+                return true;
+            }
+            Player player = (Player) sender;
             Player target = Bukkit.getPlayer(args[0]);
             if (target == null) {
                 Utils.sendColorizedMessage(player, Messages.PLAYER_NOT_FOUND);
@@ -52,14 +51,19 @@ public class Teleport extends ModuleBase {
             Player target2 = Bukkit.getPlayer(args[1]);
 
             if (target1 == null || target2 == null) {
-                Utils.sendColorizedMessage(player, Messages.PLAYER_NOT_FOUND);
+                Utils.sendColorizedMessage(sender, Messages.PLAYER_NOT_FOUND);
                 return true;
             } else {
                 target1.teleport(target2);
-                Utils.sendColorizedMessage(player, Messages.TELEPORT_SUCCESS_OTHERS.replace("{player}", target1.getName()).replace("{target}", target2.getName()));
+                Utils.sendColorizedMessage(sender, Messages.TELEPORT_SUCCESS_OTHERS.replace("{player}", target1.getName()).replace("{target}", target2.getName()));
                 return true;
             }
         } else if (args.length == 3) {
+            if (!(sender instanceof Player)) {
+                Utils.sendColorizedMessage(sender, Messages.NO_CONSOLE);
+                return true;
+            }
+            Player player = (Player) sender;
             if (!(Utils.isNumeric(args[0]) || Utils.isNumeric(args[1]) || Utils.isNumeric(args[2]))) {
                 Utils.sendColorizedMessage(player, Messages.TELEPORT_ARGS_NOT_NUMBER);
                 return true;
@@ -71,16 +75,16 @@ public class Teleport extends ModuleBase {
         } else if (args.length == 4) {
             Player target = Bukkit.getPlayer(args[0]);
             if (target == null) {
-                Utils.sendColorizedMessage(player, Messages.PLAYER_NOT_FOUND);
+                Utils.sendColorizedMessage(sender, Messages.PLAYER_NOT_FOUND);
                 return true;
             }
             if (!(Utils.isNumeric(args[1]) || Utils.isNumeric(args[2]) || Utils.isNumeric(args[3]))) {
-                Utils.sendColorizedMessage(player, Messages.TELEPORT_ARGS_NOT_NUMBER);
+                Utils.sendColorizedMessage(sender, Messages.TELEPORT_ARGS_NOT_NUMBER);
                 return true;
             }
             Location location = new Location(target.getWorld(), Integer.parseInt(args[1]), Integer.parseInt(args[2]), Integer.parseInt(args[3]));
             target.teleport(location);
-            Utils.sendColorizedMessage(player, Messages.TELEPORT_SUCCESS_OTHERS_LOCATION.replace("{player}", target.getName()).replace("{x}", args[1]).replace("{y}", args[2]).replace("{z}", args[3]));
+            Utils.sendColorizedMessage(sender, Messages.TELEPORT_SUCCESS_OTHERS_LOCATION.replace("{player}", target.getName()).replace("{x}", args[1]).replace("{y}", args[2]).replace("{z}", args[3]));
             return true;
         }
         return true;
